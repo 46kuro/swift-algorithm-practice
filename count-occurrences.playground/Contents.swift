a173146355a81018ff113e7098d71efa3bae91e2
shinji46kuro <shinji.46.kuro@gmail.com>
2020-06-16 21:11:49 +0900

    Implement Current Occurences

--- count-occurrences.playground/Contents.swift
+++ count-occurrences.playground/Contents.swift
@@ -4,5 +4,34 @@ let a = [ 0, 1, 1, 3, 3, 3, 3, 6, 8, 10, 11, 11 ]
 countOccurrences(of: 3, in: a) // gives 4
 
 func countOccurrences<T: Comparable>(of key: T, in array: [T]) -> Int {
-    array.filter { $0 == key }.count
+
+    var leftBoundaryIndex: Int {
+        var min = 0
+        var max = array.count
+        while min < max {
+            let mid = (max + min) / 2
+            if array[mid] < key {
+                min = mid + 1
+            } else {
+                max = mid
+            }
+        }
+        return min
+    }
+
+    var rightBoundaryIndex: Int {
+        var min = 0
+        var max = array.count
+        while min < max {
+            let mid = (max + min) / 2
+            if array[mid] > key {
+                max = mid
+            } else {
+                min = mid + 1
+            }
+        }
+        return min
+    }
+
+    return rightBoundaryIndex - leftBoundaryIndex
 }
