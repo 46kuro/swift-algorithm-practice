19a05e114ef3703f36fa5dd091229b2929fc82c1
shinji46kuro <shinji.46.kuro@gmail.com>
2020-06-16 20:52:52 +0900

    Count Occurrence(Brute Force)

--- count-occurrences.playground/Contents.swift
+++ count-occurrences.playground/Contents.swift
@@ -1,4 +1,8 @@
 import UIKit
 
 let a = [ 0, 1, 1, 3, 3, 3, 3, 6, 8, 10, 11, 11 ]
-countOccurrences(of: 3, in: a)
+countOccurrences(of: 3, in: a) // gives 4
+
+func countOccurrences<T: Comparable>(of key: T, in array: [T]) -> Int {
+    array.filter { $0 == key }.count
+}
